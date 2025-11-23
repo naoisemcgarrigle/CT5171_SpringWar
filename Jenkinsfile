263f73cd1da728c5aa740df7b762983fe3129405
naoisemcgarrigle <nmgarr@outlook.com>
2025-11-23 17:18:14 +0000

    Commit 9 (Docker deployment added)

--- Jenkinsfile
+++ Jenkinsfile
@@ -32,5 +32,12 @@ pipeline {
                     artifacts: '**/demo*.war'
             }
         }
+        stage('Deploy') {
+            steps {
+                sh "docker build -f Dockerfile -t myapp . "
+                sh "docker rm -f \"myappcontainer\" || true"
+                sh "docker run --name \"myappcontainer\" -p 8081:8080 --detach myapp:latest"
+            }
+        }
     }
 }
--- src/main/java/dev/naoisemcg/CT5171_SpringWar/Ct5171SpringWarApplication.java
+++ src/main/java/dev/naoisemcg/CT5171_SpringWar/Ct5171SpringWarApplication.java
@@ -2,9 +2,22 @@ package dev.naoisemcg.CT5171_SpringWar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Ct5171SpringWarApplication {
+
+    @RequestMapping("/")
+    public String hello1() {
+        return "Hello";
+    }
+
+    @RequestMapping("/hello")
+    public String hello2() {
+        return "Hello again";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ct5171SpringWarApplication.class, args);
